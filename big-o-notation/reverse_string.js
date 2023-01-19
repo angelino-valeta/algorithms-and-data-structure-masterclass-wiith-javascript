815dcd2acc50b55b13481f492283b86849bef8d1
Angelino Valeta <angelinovaleta@gmail.com>
2023-01-19 23:56:29 +0100

    Implement a function

--- big-o-notation/reverse_string.js
+++ big-o-notation/reverse_string.js
@@ -3,7 +3,14 @@ function reverse(s){
 }
 
 function reverse2(s){
-  
+  let i = s.length
+  let o = ''
+
+  while (i > 0){
+    o += s.substring(i -1, i)
+    i--
+    return o
+  }
 }
 var nome = 'Angelino';
 
